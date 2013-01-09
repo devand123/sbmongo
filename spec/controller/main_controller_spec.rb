da6a36f2a3933e6da692629d666ccdbd31231294
thejourneydude <journeyofthedude@gmail.com>
2013-01-08 19:00:29 -0500

    Updated rspec testing

--- spec/controller/main_controller_spec.rb
+++ spec/controller/main_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 
 
-describe 'MyApp' do
+describe 'Mongo SB' do
   include Rack::Test::Methods
 
   def app
@@ -11,11 +11,50 @@ describe 'MyApp' do
 
 context "home page" do
 
-  it "should display appropriate content" do
+  subject { last_response }
+
+  it "should display header" do
     get '/'
-    last_response.should be_ok
     last_response.body.should include("Mongo Sandbox")
   end
+
+
+  it { last_response.should render_template(:index) }
+
+end
+
+context "new movie" do
+
+  it "page should display header" do
+    get '/new'
+    last_response.body.should include ("New Movie")
+  end
+end
+
+context "show movie" do
+
+  it "page should display header" do
+    get '/new'
+    last_response.body.should include ("New Movie")
+  end
+end
+
+context "destroy movie" do
+
+  it "page should display header" do
+    get '/new'
+    last_response.body.should include ("New Movie")
+  end
+  end
+
+context "edit movie" do
+
+  it "page should display header" do
+    get '/new'
+    last_response.body.should include ("New Movie")
+  end
+end
+
 
 
 
--- spec/routes/main_routes_spec.rb
+++ spec/routes/main_routes_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 
 
-describe 'MyApp' do
+describe 'Mongo SB' do
   include Rack::Test::Methods
 
   def app
@@ -11,6 +11,8 @@ describe 'MyApp' do
 
 context "routes" do
 
+  subject { last_response }
+
   it 'should return a 200 on the home page' do
     get '/'
     last_response.status.should == 200
@@ -18,11 +20,74 @@ context "routes" do
 
   it 'should return ok on the home page' do
     get '/'
-    last_response.should be_ok
+    should be_ok
   end
 
 
-  
+  context "for movies" do
+
+    it 'should return a 200 on the new page' do
+      get '/new'
+      last_response.status.should == 200
+    end
+
+    it 'should return ok on the new page' do
+      get '/new'
+      should be_ok
+    end
+
+    it 'should return a 200 on the create page' do
+      post '/movies'
+      last_response.status.should == 200
+    end
+
+    it 'should return ok on the create page' do
+      post '/movies'
+      should be_ok
+    end
+
+    it 'should return a 200 on the get page' do
+      get '/movie/' + @movie
+      last_response.status.should == 200
+    end
+
+    it 'should return ok on the get page' do
+      get '/movie/' + @movie
+      should be_ok
+    end
+
+    it 'should return a 200 on the post page' do
+      put '/movie/' + @movie
+      last_response.status.should == 200
+    end
+
+    it 'should return ok on the post page' do
+      put '/movie/' + @movie
+      should be_ok
+    end
+
+    it 'should return a 200 on the delete page' do
+      delete '/movie/' + @movie
+      last_response.status.should == 200
+    end
+
+    it 'should return ok on the delete page' do
+      delete '/movie/' + @movie
+      should be_ok
+    end
+
+    it 'should return a 200 on the edit page' do
+      delete '/movie/' + @movie + '/edit'
+      last_response.status.should == 200
+    end
+
+    it 'should return ok on the edi page' do
+      delete '/movie/' + @movie + '/edit'
+      should be_ok
+    end
+
+  end
+
 end
 
 end
