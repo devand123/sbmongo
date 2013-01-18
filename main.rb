ba9c5c111df03df69574b17be8e9d157dc0e4647
thejourneydude <journeyofthedude@gmail.com>
2013-01-18 00:37:52 -0500

    took out user system for implicit collection of ip addresses only on update or create document

--- main.rb
+++ main.rb
@@ -31,29 +31,16 @@ class Document
   key :author, String, :required => true
   key :number_of_edits, Integer, :required => true
   key :contributor_ids, Array
-  many :users, :in => :contributor_ids
   timestamps!
 
   attr_accessible :title, :body, :author, :number_of_edits
 
 end
 
-class User
-
-  include MongoMapper::Document
-
-  key :user_id, String, :required => true, :unique => true
-  key :name, String, :required => true
-  timestamps!
-
-end
 
 
 #Helpers Section
 #===============
-first_name = ["slick", "windy", "velvet", "micro"]
-
-second_name = ["dinosaur", "poet", "lion", "writer", "autodidact"]
 
 
 def token_from_form_verified()
@@ -67,56 +54,24 @@ end
 enable :sessions
 
   get '/' do
-    @users = User.all
     @documents = Document.all
     slim :index
   end
 
-
-
   get '/edit/:id' do
     document = Document.find(params[:id])
     @document = document
-    @body = document.body.join(".") + "."
+    @body = document.body.join(".")
     slim :edit
   end
 
-  get '/login' do
-    session[:remote_ip] =  request.env['REMOTE_ADDR'].split(',').first
-    user = User.find({user_id: session[:remote_ip]})
-    if user.nil?
-      user = User.new
-      user.user_id = session[:remote_ip]
-      user.name = first_name.sample + "-" + second_name.sample + "-" + session[:remote_ip]
-      if user.save
-        session[:user_name] = user.name
-        status 202
-      else
-        status 401
-      end
-    else
-      session[:user_name] = user.name
-    end
-    redirect '/'
-  end
-
   get '/new' do
-    if session[:user_name].nil?
-      redirect '/login'
-    else
-      slim :new
-    end
+    slim :new
   end
-
-
 
   post '/create' do
 
     token_from_form_verified()
-
-    if session[:user_name].nil?
-      redirect '/login'
-    end
 
     #Injecting Document Attributes into new Document
     document = Document.new
@@ -124,7 +79,7 @@ enable :sessions
     document.body = (params[:body]).split(".")
     document.author = (params[:author])
     document.number_of_edits = 1
-    document.contributor_ids = session[:remote_ip]
+    document.contributor_ids = request.env['REMOTE_ADDR'].split(',').first
 
     #Saving Document
     if document.save
@@ -141,17 +96,12 @@ enable :sessions
 
     token_from_form_verified()
 
-
-    if session[:user_name].nil?
-      redirect '/login'
-    end
-
     document = Document.find( params[:id] )
     document.title = ( params[:title] )
     document.body = ( params[:body] )
     document.author = ( params[:author] )
     document.number_of_edits += 1
-    document.add_to_set( :contributor_ids => session[:remote_ip] )
+    document.add_to_set( :contributor_ids => request.env['REMOTE_ADDR'].split(',').first )
 
     if document.update_attributes!
       status 201
